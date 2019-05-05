06dbcc039255fcfbb90264fe162451897bc60a89
MinyoungKoo <rnalsdud1014@naver.com>
2019-05-06 03:06:33 +0900

    공유하기 버튼 클릭 시 카카오톡으로 공유

--- Toy-Contest/Sources/ViewControllers/ResultVC.swift
+++ Toy-Contest/Sources/ViewControllers/ResultVC.swift
@@ -9,11 +9,53 @@
 import UIKit
 
 class ResultVC: UIViewController {
-
+    @IBOutlet weak var shareBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        shareBtn.addTarget(self, action: #selector(tapShareBtn), for: .touchUpInside)
+    }
+    
+    @objc func tapShareBtn() {
+        // Feed 타입 템플릿 오브젝트 생성
+        let template = KMTFeedTemplate { (feedTemplateBuilder) in
+            
+            // 컨텐츠
+            feedTemplateBuilder.content = KMTContentObject(builderBlock: { (contentBuilder) in
+                contentBuilder.title = "장난감이름장난감이름장난감이름"
+                contentBuilder.desc = "나 이거 사죠!!!!!!!!!!!!!!!!!!!!!!!"
+                contentBuilder.imageURL = URL(string: "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png")!
+                contentBuilder.link = KMTLinkObject(builderBlock: { (linkBuilder) in
+                    linkBuilder.mobileWebURL = URL(string: "https://developers.kakao.com")
+                })
+            })
+            
+            // 버튼
+            feedTemplateBuilder.addButton(KMTButtonObject(builderBlock: { (buttonBuilder) in
+                buttonBuilder.title = "자세히 보기"
+                buttonBuilder.link = KMTLinkObject(builderBlock: { (linkBuilder) in
+                    linkBuilder.mobileWebURL = URL(string: "https://developers.kakao.com")
+                })
+            }))
+        }
+        
+        // 카카오링크 실행
+        KLKTalkLinkCenter.shared().sendDefault(with: template, success: { (warningMsg, argumentMsg) in
+            
+            // 성공
+            print("warning message: \(String(describing: warningMsg))")
+            print("argument message: \(String(describing: argumentMsg))")
+            
+        }, failure: { (error) in
+            
+            // 실패
+            print("error \(error)")
+            
+        })
     }
     
 
